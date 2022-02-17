303ce029a8219fd2cc6b811b8b9fe42d7320a56f
aleshataguch <alesha.taguch@gmail.com>
2022-02-17 23:00:57 +0500

    movеd project from another repository

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Убрир on 13.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
