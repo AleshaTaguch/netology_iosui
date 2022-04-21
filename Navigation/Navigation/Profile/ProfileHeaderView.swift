bf9927ff2a109b69b9e15f44d0c91eb4be1a7fd7
AleshaTaguch <alesha.taguch@gmail.com>
2022-04-21 19:36:00 +0500

    start task

--- Navigation/Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Navigation/Profile/ProfileHeaderView.swift
@@ -105,8 +105,6 @@ extension ProfileHeaderView {
     
     @objc func tapedAvatarImageView(_ recognizer: UITapGestureRecognizer) {
         
-        print("======================== tapedAvatarImageView")
-        
         let userInfoNatification: [String : Any] = ["fullScreenAvatarView":fullScreenAvatarView]
         let notificationCenter = NotificationCenter.default
         notificationCenter.post(name: Notification.Name(ProfileFullScreenAvatarView.tapShowAvatar),
